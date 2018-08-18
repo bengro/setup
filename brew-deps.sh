3e2d7b228b487ebba9423af73deb3ff59f6cdbe5
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-08-18 13:38:55 +0100

    Add vim, brew and iterm configuration

---
+++ brew-deps.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install lastpass-cli wget tree node kubectl terraform vim
+brew cask install google-backup-and-sync intellij-idea google-chrome iterm2 flycut spectacle spotify evernote steam docker mysqlworkbench cyberduck fly
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
---
+++ configure-vim.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mv ~/.vim ~/.vim.backup
+git clone git@github.com:bengro/vim-config.git ~/.vim
