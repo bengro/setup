f817210efcf48c34d2400827f4057b57e6f23ae4
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-09-09 10:46:57 +0100

    Add tmate, ack and direnv to dependencies

--- brew-deps.sh
+++ brew-deps.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install lastpass-cli wget tree node kubectl terraform vim pwgen htop tig
+brew install lastpass-cli wget tree node kubectl terraform vim pwgen htop tig tmate ack direnv
 brew cask install google-backup-and-sync intellij-idea google-chrome iterm2 flycut spectacle spotify evernote steam docker mysqlworkbench cyberduck fly
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
