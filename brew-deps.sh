7c3f863600db9aff0d570b8e838c0ce52f8271cf
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-09-29 17:27:27 +0100

    Add java8, java10 and git

--- brew-deps.sh
+++ brew-deps.sh
@@ -2,8 +2,8 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install lastpass-cli wget tree node kubectl terraform vim pwgen htop tig tmate ack direnv jq
-brew cask install google-backup-and-sync intellij-idea google-chrome iterm2 flycut spectacle spotify evernote steam docker mysqlworkbench cyberduck fly
+brew install lastpass-cli wget tree node kubectl terraform vim pwgen htop tig tmate ack direnv jq git
+brew cask install google-backup-and-sync intellij-idea google-chrome iterm2 flycut spectacle spotify evernote steam docker mysqlworkbench cyberduck fly java8 java10
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
